7fe3da8f72efb8003e816e59a6bd06d540cf3b5b
yohanes_prima <jooprima@gmail.com>
2019-07-14 23:15:06 +0700

    Tugas 15 js
    tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas15.js
@@ -0,0 +1,9 @@
+function pytagoras() {
+  var x = 8;
+  var y = 6;
+  var z = Math.sqrt(Math.pow(8, 2) + Math.pow(6, 2));
+  console.log("Nilai AB : " + x);
+  console.log("Nilai BC : " + y);
+  console.log("Panjang sisi AC pada segitiga siku-siku tersebut adalah : " + z);
+}
+pytagoras();
